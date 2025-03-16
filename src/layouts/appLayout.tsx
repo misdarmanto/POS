5f2f241633a1ae0549e821ed4ddbb79b70a5c6df
misdarmanto <misdarmanto1922@gmail.com>
2025-03-16 11:32:03 +0700

    feat: ceate bottom navigation

--- src/layouts/appLayout.tsx
+++ src/layouts/appLayout.tsx
@@ -7,14 +7,6 @@ import Logo from "../assets/logo.png";
 const AppLayout: React.FC = () => {
   return (
     <div className="layout">
-      <AppBar
-        logo={Logo}
-        links={[
-          { name: "Home", href: "#" },
-          { name: "Profile", href: "#" },
-        ]}
-      />
-
       <div className="content">
         <Outlet />
       </div>
