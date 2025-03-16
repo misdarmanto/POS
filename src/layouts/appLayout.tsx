e8999ef4ddacd6da2cc9caef5a3e13772e04da34
misdarmanto <misdarmanto1922@gmail.com>
2025-03-16 11:53:15 +0700

    fix: layout

--- src/layouts/appLayout.tsx
+++ src/layouts/appLayout.tsx
@@ -3,13 +3,23 @@ import AppBar from "../components/appBar";
 import "./Layout.css";
 import { Outlet } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import BottomMenu from "../components/buttomMenu";
+import { IoHome, IoTime, IoPerson } from "react-icons/io5";
 
 const AppLayout: React.FC = () => {
+  const menuItems = [
+    { name: "Home", path: "/", icon: <IoHome size={24} /> },
+    { name: "History", path: "/history", icon: <IoTime size={24} /> },
+    { name: "Profile", path: "/profile", icon: <IoPerson size={24} /> },
+  ];
+
   return (
     <div className="layout">
+      <AppBar logo={Logo} links={[{ name: "Home", href: "#" }]} />
       <div className="content">
         <Outlet />
       </div>
+      <BottomMenu items={menuItems} />
     </div>
   );
 };
